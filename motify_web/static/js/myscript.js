01e5316d7e27f1c1445dfd1db62367487b10934d
jesiel364 <jesiel364@gmail.com>
2022-11-15 23:42:55 -0400

    'as'

--- motify_web/static/js/myscript.js
+++ motify_web/static/js/myscript.js
@@ -1,3 +1,32 @@
-function remove_flash(){
-    console.alert("Função Disparada!")
+// var url = 'https://api.adviceslip.com/advice'
+// var request = new XMLHttpRequest();
+
+// fetch(url, {
+//   method: 'GET'
+// }).then((response) => {
+//   response.json().then((jsonResponse) => {
+//     console.log(jsonResponse)
+//   })
+//   // assuming your json object is wrapped in an array
+//   response.json().then(i => i.forEach(i => console.log(i.advice)))
+// }).catch((err) => {
+//   console.log(`Error: ${err}` )
+// });
+    
+
+
+
+var json = '{"nome": "Jesiel", "idade": "22"}'
+
+var obj = JSON.parse(json)
+
+console.log(obj.nome)
+
+function copy() {
+  var text = document.getElementsByClassName("card-text");
+navigator.clipboard.writeText(text).then(function() {
+  console.log(text.value);
+}, function(err) {
+  console.error('Async: Could not copy text: ', err);
+});
 }
