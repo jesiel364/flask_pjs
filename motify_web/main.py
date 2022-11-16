4605af787ed545ec85e5e08c98d6ca16aa9316a1
jesiel364 <jesiel364@gmail.com>
2022-11-15 20:26:25 -0400

    b

--- motify_web/main.py
+++ motify_web/main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
+import requests
 
 app = Flask('__main__')
 
 @app.route('/')
 def index():
-    return render_template('/home.html')
+    advice = requests.get('https://api.adviceslip.com/advice').json()
+    for key in advice:
+        conselho = advice.get(key)['advice']
+        print(conselho)
+    return render_template('/home.html', data = conselho)
     
 if __name__ == '__main__':
     app.run(debug=True, port=8089)
