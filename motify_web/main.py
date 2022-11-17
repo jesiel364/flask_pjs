128aa8b8c2d8f73769ab30a33cfca441f09953c6
jesiel364 <jesiel364@gmail.com>
2022-11-16 21:43:14 -0400

    ahhk

--- motify_web/main.py
+++ motify_web/main.py
@@ -8,12 +8,18 @@ def index():
     advice = requests.get('https://api.adviceslip.com/advice').json()
     for key in advice:
         conselho = advice.get(key)['advice']
-        print(conselho)
-    return render_template('/home.html', data = conselho)
+        key_id = advice.get(key)['id']
+        print(conselho, key_id)
+    return render_template('/home.html', conselho = conselho, key_id = key_id)
     
 @app.route('/salvos')
 def salvos():
     return render_template('/salvos.html')
+    
+@app.route('/salvar/<string:key>')
+def salvar(key):
+    print(key)
+    return redirect(url_for('index'))
     
 if __name__ == '__main__':
     app.run(debug=True, port=8089)
