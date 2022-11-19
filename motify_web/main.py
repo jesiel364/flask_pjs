96b494cf015cb6ef1fd06561439800476885f51b
jesiel364 <jesiel364@gmail.com>
2022-11-19 13:15:02 -0400

    ajhga

--- motify_web/main.py
+++ motify_web/main.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 import requests
+import sqlite3
+
+
 
 app = Flask('__main__')
+
 
 @app.route('/')
 def index():
@@ -39,12 +43,71 @@ def login():
 @app.route('/registrar', methods=['POST', 'GET'])
 def registrar():
     if request.method == 'POST':
+
+        #Register
+        name = request.form['nick']
+        newEmail = request.form['newEmail']
+        newPwd = request.form['newPwd']
+        newPwd2 = request.form['newPwd2']
+        dados = name, newEmail, newPwd
+
+        try:
+            if newPwd == newPwd2:
+                conn = sqlite3.connect('users.db')
+                cursor = conn.cursor()
+                cursor.execute('''
+                INSERT INTO users (nome, email, senha)
+                VALUES (?,?,?)
+                ''',dados)
+                conn.commit()
+                flash(f'Sua conta foi criada com sucesso!', 'success')
+            else:
+                flash('As senhas não são iguais!', 'danger')
+        except:
+            flash('Não foi possivel completar o cadastro', 'danger')
+                
+
+            
+        return redirect(url_for('login'))
+
+
+
+
         
+    
+
+@app.route('/entrar', methods=['POST', 'GET'])
+def entrar():
+    if request.method == 'POST':
+        
+        #Login
         email = request.form['email']
         pwd = request.form['pwd']
-        print(email, pwd)
+        dados = email, pwd
+        print(dados)
+
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT senha FROM users where email=?', (email,))
+        # flash('Usuário logado', 'success')
+        # senha = str(cursor.fetchone(3))
+        
+        if cursor.fetchone()[0] == pwd:
+        # else:
+            flash('logado', 'light')
+        else:
+            flash('Não encontrado', 'light')
+
+
+
+        # except:
+        #     flash('Usuário não encontrado', 'danger')
+        #     return redirect(url_for('login'))
+
+
     return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+    app.secret_key='12345'
     app.run(debug=True, port=8089)
